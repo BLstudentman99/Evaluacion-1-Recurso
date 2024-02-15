9091a4fcc9ac48856508b9ad889af51c8d0446cc
BLstudentman99 <143562661+BLstudentman99@users.noreply.github.com>
2024-02-15 16:46:14 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACIÓN/src/eva1_4_asignación/EVA1_4_ASIGNACIÓN.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_asignación;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_ASIGNACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad; //declacarión
+        String nombre;
+        String carrera;
+        nombre = "Erick Eduardo Anchondo Portillo";
+        carrera = "Ingenieria en sistemas computacionales";
+        edad = 18; //asignación: colocar valor dentro de la variable    
+        
+        System.out.println("tu nombre es: " + nombre + ", " + "tienes nombre comun!");
+        System.out.println("tu edad es: " + edad + ", " + "apenas eres legal!");
+        System.out.println("tu carrera es: " + carrera + " " + "Estás en una carrera dificil!");
+  
+                
+    }
+    
+}
